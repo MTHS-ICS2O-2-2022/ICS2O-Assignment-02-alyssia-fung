332d660c6bf1d9c2b1dbb547a3d06e11ce78576b
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-03-28 16:21:56 +0000

    area of a circle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function calculates area of a circle
+ */
+"use strict"
+
+function calculate() {
+  // input
+  const radius = parseInt(document.getElementById("radius").value)
+
+  // process
+  const area = 3.14 * radius ** 2
+
+  // output
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+}
